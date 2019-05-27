ac08ef7db5f941f61412c7e323a54caea4503c23
soto92 <mauriciosoto@rede.ulbra.br>
2019-05-27 14:12:39 -0300

    alterado checkbox e cores dos inbox quando selecionado.utilizado jquery)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
+$("#square").click(function () {
+  if ($(this).attr("src") == "assets/images/icons/check_box_outline_blank_black_20dp.png") {
+    $(this).attr("src", "assets/images/icons/mark-check.png");
+    $(".square").attr("src", "assets/images/icons/mark-check.png").css("opacity", "1.2");
+    $(".tr").css('background', '#c2dbff');
+  } else {
+    $(this).attr("src", "assets/images/icons/check_box_outline_blank_black_20dp.png");
+    $(".square").attr("src", "assets/images/icons/check_box_outline_blank_black_20dp.png").css("opacity", "0.2");
+    $(".tr").css('background', '#ffffff');
+  }
+});
+
+
 let months = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
-let emails = [
-  {
+let emails = [{
     name: faker.name.findName(),
     subject: faker.random.words(3),
     preview: faker.random.words(6),
@@ -40,83 +52,49 @@ let emails = [
 
 const emailPreview = document.getElementsByClassName("email-list");
 
-$("a.write-email").click(function() {
+$("a.write-email").click(function () {
   $("section.new-message").removeClass("invisible");
 });
 
-$("li.new-message-close").click(function() {
+$("li.new-message-close").click(function () {
   $("section.new-message").addClass("invisible");
 });
 
-$("label.search-email-box").click(function() {
+$("label.search-email-box").click(function () {
   $(this).addClass("search-email-click");
   $("input.search-email").css("background-color", "white");
 });
 
-$("body").click(function(event) {
+$("body").click(function (event) {
   if (!$("label.search-email-box").is(event.target) && !$("input.search-email").is(event.target)) {
     $("label.search-email-box").removeClass("search-email-click");
     $("input.search-email").css("background-color", "#f1f3f4");
-  }});
+  }
+});
 
-const users = [{
-        nome: 'Mauricio',
-        foto: 'assets/images/users/mauricio.jpeg',
-        ultimaMensagem: 'Você: Funfa sim!',
 
-    },
-    {
-        nome: 'Tejada',
-        foto: 'assets/images/users/tejada.png',
-        ultimaMensagem: 'Você: Sou backend :P',
+$("a.write-email").click(function () {
 
-    },
-    {
-        nome: 'Bean',
-        foto: 'assets/images/users/bean.jpeg',
-        ultimaMensagem: 'Você: Hello XD',
-    } 
-]
-
-let html = '';
-
-users.forEach(user => {
-    html += `
-<div class="hangout-talk-profile">
-        <img src=${user.foto} title="User" alt="User" class="profile-photo" />
-        <div class="hangout-users-info">
-          <p class="profile-name">${user.nome}</p>
-          <span class="profile-message">${user.ultimaMensagem}</span>
-        </div>
-      </div>
-  `
-})
-
-const divContatos = document.getElementById('hangout-talk');
-divContatos.innerHTML = html;
-
-$("a.write-email").click(function() {
-
-    $("section.new-message").removeClass("invisible");
+  $("section.new-message").removeClass("invisible");
 });
 
 $("li.new-message-close").click(function () {
-    $("section.new-message").addClass("invisible");
+  $("section.new-message").addClass("invisible");
 });
 
 $("label.search-email-box").click(function () {
-    $(this).addClass("search-email-click");
-    $("input.search-email").css('background-color', 'white');
+  $(this).addClass("search-email-click");
+  $("input.search-email").css('background-color', 'white');
 });
 
 $("body").click(function (event) {
-    if (!$("label.search-email-box").is(event.target) && !$("input.search-email").is(event.target)) {
-        $("label.search-email-box").removeClass("search-email-click");
-        $("input.search-email").css('background-color', '#f1f3f4');
-    }
+  if (!$("label.search-email-box").is(event.target) && !$("input.search-email").is(event.target)) {
+    $("label.search-email-box").removeClass("search-email-click");
+    $("input.search-email").css('background-color', '#f1f3f4');
+  }
 });
 
-$(".hangout-talk-profile").click(function() {
+$(".hangout-talk-profile").click(function () {
   $(".talk-hangout").removeClass("invisible");
 });
 
@@ -140,19 +118,19 @@ function getNewEmail() {
 function getNewEmailHtml() {
   let htmlEmails = "";
 
-  emails.sort(function(email1, email2) {
+  emails.sort(function (email1, email2) {
     return new Date(email2.date) - new Date(email1.date);
   });
 
-  emails.forEach(function(email) {
+  emails.forEach(function (email) {
     htmlEmails += `
-        <tr>
+        <tr class="tr">
             <td class="checkbox-column">
-              <img src="https://www.gstatic.com/images/icons/material/system/1x/check_box_outline_blank_black_20dp.png"
+              <img src="assets/images/icons/check_box_outline_blank_black_20dp.png" class="square"
                 title="Checkbox" alt="checkbox">
-            </td>
+            </td> 
             <td class="star-column">
-              <img src="https://www.gstatic.com/images/icons/material/system/1x/star_border_black_20dp.png" title="Star"
+              <img  src="assets/images/icons/star_border_black.png" title="Star"
                 alt="star">
             </td>
             <td class="info-column">
@@ -170,15 +148,16 @@ function getNewEmailHtml() {
   emailPreview[0].innerHTML = htmlEmails;
 }
 
-function defineDateString(date){
+function defineDateString(date) {
   let dateNow = new Date();
   let dateParsed = date.getTime();
   const diffTime = Math.abs(dateNow - dateParsed);
   const diffDays = diffTime / (1000 * 60 * 60 * 24);
-  if (diffDays < 1){
-      if(date.getMinutes() < 10)
+  if (diffDays < 1) {
+    if (date.getMinutes() < 10)
       return `${date.getHours()}:0${date.getMinutes()}`;
-    return `${date.getHours()}:${date.getMinutes()}`;}
+    return `${date.getHours()}:${date.getMinutes()}`;
+  }
   if (dateNow.getFullYear() == date.getFullYear()) return `${date.getDate()} de ${months[date.getMonth()]}`;
   return `${date.getDate()}/${date.getMonth() +
     1}/${date.getFullYear()}`;
@@ -186,3 +165,39 @@ function defineDateString(date){
 
 getNewEmail();
 getNewEmailHtml();
+
+const users = [{
+    nome: 'Mauricio',
+    foto: 'assets/images/users/mauricio.jpeg',
+    ultimaMensagem: 'Você: Funfa sim!',
+
+  },
+  {
+    nome: 'Tejada',
+    foto: 'assets/images/users/tejada.png',
+    ultimaMensagem: 'Você: Sou backend :P',
+
+  },
+  {
+    nome: 'Bean',
+    foto: 'assets/images/users/bean.jpeg',
+    ultimaMensagem: 'Você: Hello XD',
+  }
+]
+
+let html = '';
+
+users.forEach(user => {
+  html += `
+<div class="hangout-talk-profile">
+      <img src=${user.foto} title="User" alt="User" class="profile-photo" />
+      <div class="hangout-users-info">
+        <p class="profile-name">${user.nome}</p>
+        <span class="profile-message">${user.ultimaMensagem}</span>
+      </div>
+    </div>
+`
+})
+
+const divContatos = document.getElementById('hangout-talk');
+divContatos.innerHTML = html;
